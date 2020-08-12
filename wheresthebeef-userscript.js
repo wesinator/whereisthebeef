1d4dd0ea5fc8805acead624332788f93adcc9c34
Ԝеѕ <5124946+wesinator@users.noreply.github.com>
2020-08-12 04:34:19 -0400

    Create wheresthebeef-userscript.js

---
+++ wheresthebeef-userscript.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name     Where's the BeEF?
+// @author   wesinator
+// @version  1
+// @grant    none
+// ==/UserScript==
+
+function checkBeef() {
+  scripts = document.getElementsByTagName("script");
+  //console.log("wheresthebeef here");
+  
+  for (script of scripts)
+  {
+    if (script.src.includes(":3000/hook.js"))
+    {
+      displayWarning(script.src);
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+function displayWarning(string) {
+  var beefMessage = `Warning: BeEF hook script identified in scripts on page: "${string}"`;
+  console.log(beefMessage);
+  alert(beefMessage);
+}
+
+checkBeef();
